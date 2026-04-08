8c0f9043c63a78b7544ee5bfb1eb5709a1b7ec0e
Levi Soares <levisoaresp.alho.8c.m@gmail.com>
2026-04-08 13:38:23 -0300

    Melhorias na apresentação do menu inicial e na exibição do carregamento do sistema

--- Main.py
+++ Main.py
@@ -1,7 +1,10 @@
 import os
 import rich
 import time
+import sys
 from colorama import init, Fore, Style
+
+# Logotipo em forma de dragão
 
 init()
 
@@ -16,3 +19,28 @@ for cor, linha in logo:
     print(cor + linha)
 
 print(Style.RESET_ALL)
+
+print("=" * 30)
+print("Dragonic 4456 Console V0.1")
+
+
+print(" ")
+
+
+
+
+print("Olá! Bem vindo ao Dragonic 4456!")
+print("Por favor, aguarde")
+
+print(" ")
+
+for i in range(21):
+    barra = "#" * i + " " * (20 - i)
+    porcentagem = i * 5
+    sys.stdout.write(f"\r[{barra}] {porcentagem}%")
+    sys.stdout.flush()
+    time.sleep(0.15)
+
+print("\nSistema pronto!")
+
+
