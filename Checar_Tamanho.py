b25f6d5963dbd26bf914b6891db734df7ef0a86b
Levi Soares <levisoaresp.alho.8c.m@gmail.com>
2026-04-08 12:58:54 -0300

    Adição de sistema if/else

--- Checar_Tamanho.py
+++ Checar_Tamanho.py
@@ -1,5 +1,20 @@
 import os
 import Main as M
+import time
 
 
 caminho = M.Path  # ainda não foi adicionado no arquivo Main uma forma de carregar.
+
+tamanho = os.stat(caminho).st_size
+
+for i in range(20):
+    os.system("cls" if os.name == "nt" else "clear")
+    print("carregando modelo" + "." * (i + 1))
+    time.sleep(0.5)
+
+if tamanho <= 900000000:
+    pass
+else:
+    print("Erro, o arquivo é muito grande e não será usado.")
+    exit()
+
