b098ae37d03437e632355be6d50fe1bd64e3abe5
Levi Soares <levisoaresp.alho.8c.m@gmail.com>
2026-04-08 12:41:56 -0300

    Carrega o modelo e deixa pronto para o uso

--- Carregar_Modelo.py
+++ Carregar_Modelo.py
@@ -5,3 +5,5 @@ import Main as M
 
 caminho = M.Path # ainda não foi adicionado no arquivo Main uma forma de carregar.
 
+#Usa o modelo torchscript para carregar
+modelo = torch.load(caminho, map_location="cpu")
