65ea65622b65f778ee4b6d9a4b965fe4e28124c3
Levi Soares <levisoaresp.alho.8c.m@gmail.com>
2026-04-09 14:15:18 -0300

    Adição da função carregar_modelo em Carregar_Modelo.py para carregar o modelo LLaMA com parâmetros configuráveis.

---
+++ Carregar_Modelo.py
@@ -0,0 +1,11 @@
+from llama_cpp import Llama # Chama é uma biblioteca de código aberto para trabalhar com modelos de linguagem, como o LLaMA da Meta. A classe Llama é usada para carregar e interagir com esses modelos.
+
+# Função que carrega o modelo
+def carregar_modelo(caminho):
+    llm = Llama(
+        model_path=caminho,
+        n_ctx=600, # contexto de 600 tokens 
+        verbose=False # desativa mensagens de log detalhadas
+    )
+    return llm
+
